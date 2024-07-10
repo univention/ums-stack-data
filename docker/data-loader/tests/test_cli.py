199917d1da0aaf2b502cbc49b0aa700e06294b7f
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-10 07:54:23 +0200

    fix: Accept the filename to process as cli argument

--- docker/data-loader/tests/test_cli.py
+++ docker/data-loader/tests/test_cli.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import pytest
 from typer.testing import CliRunner
 
@@ -6,4 +8,14 @@ runner = CliRunner()
 
 def test_shows_help_information(process_join_data):
     result = runner.invoke(process_join_data.cli_app, ["--help"])
+    assert result.exit_code == 0
+
+
+def test_accepts_filename_as_argument(process_join_data, mocker):
+    mocker.patch.object(process_join_data, "_connect_to_udm")
+    run_mock = mocker.patch.object(process_join_data.App, "run")
+
+    result = runner.invoke(process_join_data.cli_app, ["example-data.yaml"])
+
+    run_mock.assert_called_once_with("example-data.yaml")
     assert result.exit_code == 0
