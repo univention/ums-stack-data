61a70afc1648ab8c622ac9c69bbab5ce537861fc
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-10 07:20:29 +0200

    refactor: Move fixture "process_join_data" into conftest module

--- docker/data-loader/tests/test_cli.py
+++ docker/data-loader/tests/test_cli.py
@@ -1,25 +1,7 @@
-import importlib.util
-import os
-import os.path
-
 import pytest
 from typer.testing import CliRunner
 
 runner = CliRunner()
-
-
-@pytest.fixture
-def process_join_data():
-    """Provide "process-join-data.py" as a module."""
-    module_name = "process_join_data"
-    module_path = "./bin/process-join-data.py"
-    spec = importlib.util.spec_from_file_location(
-        module_name,
-        os.path.join(module_path),
-    )
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
 
 
 def test_shows_help_information(process_join_data):
