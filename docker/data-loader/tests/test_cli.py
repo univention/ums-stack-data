77ffab1bf4d68db1b79792fb5d6e4929d316cc78
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-12 11:03:59 +0200

    feat: Add support for UDM connection configuration into CLI
    This keeps the usage of environment variables as before and does not replace it.

--- docker/data-loader/tests/test_cli.py
+++ docker/data-loader/tests/test_cli.py
@@ -8,6 +8,13 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
+DEFAULT_ENV = {
+    "UDM_API_URL": "stub_url",
+    "UDM_API_USER": "stub_user",
+    "UDM_API_PASSWORD_FILE": "stub_password",
+}
+
+
 def test_shows_help_information(process_join_data):
     result = runner.invoke(process_join_data.cli_app, ["--help"])
     assert result.exit_code == 0
@@ -17,7 +24,11 @@ def test_accepts_filename_as_argument(process_join_data, mocker):
     mocker.patch.object(process_join_data, "_connect_to_udm")
     run_mock = mocker.patch.object(process_join_data.App, "run")
 
-    result = runner.invoke(process_join_data.cli_app, ["example-data.yaml"])
+    result = runner.invoke(
+        process_join_data.cli_app,
+        ["example-data.yaml"],
+        env=DEFAULT_ENV,
+    )
 
     run_mock.assert_called_once_with("example-data.yaml")
     assert result.exit_code == 0
@@ -30,6 +41,7 @@ def test_accepts_template_extension_as_option(process_join_data, mocker):
     result = runner.invoke(
         process_join_data.cli_app,
         ["example-data.yaml", "--template-extension", ".j2"],
+        env=DEFAULT_ENV,
     )
 
     assert app_mock.call_args.kwargs["template_extension"] == ".j2"
@@ -49,6 +61,7 @@ def test_accepts_template_context_as_option(process_join_data, mocker):
     result = runner.invoke(
         process_join_data.cli_app,
         ["example-data.yaml", "--template-context", "context.yaml"],
+        env=DEFAULT_ENV,
     )
 
     load_context_mock.assert_called_once_with("context.yaml")
@@ -76,6 +89,7 @@ def test_accepts_multiple_template_context_options(process_join_data, mocker):
             "--template-context",
             "context2.yaml",
         ],
+        env=DEFAULT_ENV,
     )
 
     expected_context = stub_context | stub_context2
@@ -103,6 +117,7 @@ def test_does_not_log_context_by_default(process_join_data, mocker, caplog):
                 "--template-context",
                 "context2.yaml",
             ],
+            env=DEFAULT_ENV,
         )
 
     assert "Merged context" not in caplog.text
@@ -130,7 +145,73 @@ def test_allows_to_log_the_context(process_join_data, mocker, caplog):
                 "--template-context",
                 "context2.yaml",
             ],
+            env=DEFAULT_ENV,
         )
 
     assert "Merged context" in caplog.text
+    assert result.exit_code == 0
+
+
+def test_allows_to_configure_the_udm_connection_via_cli(process_join_data, mocker):
+    udm_api_url = "stub_udm_api_url"
+    udm_api_user = "stub_udm_api_user"
+    udm_api_password = "stub_udm_api_password"
+    udm_api_password_file = "stub_udm_api_password_file"
+    udm_mock = mocker.patch.object(process_join_data, "UDM")
+    read_from_file_mock = mocker.patch.object(
+        process_join_data,
+        "read_from_file",
+        return_value=udm_api_password,
+    )
+    mocker.patch.object(process_join_data, "App")
+
+    result = runner.invoke(
+        process_join_data.cli_app,
+        [
+            "example-data.yaml",
+            "--udm-api-url",
+            udm_api_url,
+            "--udm-api-user",
+            udm_api_user,
+            "--udm-api-password-file",
+            udm_api_password_file,
+        ],
+        catch_exceptions=False,
+    )
+
+    read_from_file_mock.assert_called_once_with(udm_api_password_file)
+    udm_mock.http.assert_called_once_with(udm_api_url, udm_api_user, udm_api_password)
+    assert result.exit_code == 0
+
+
+def test_configures_udm_connection_based_on_environment_variables(
+    process_join_data,
+    mocker,
+):
+    udm_api_url = "stub_udm_api_url"
+    udm_api_user = "stub_udm_api_user"
+    udm_api_password = "stub_udm_api_password"
+    udm_api_password_file = "stub_udm_api_password_file"
+    env = {
+        "UDM_API_URL": udm_api_url,
+        "UDM_API_USER": udm_api_user,
+        "UDM_API_PASSWORD_FILE": udm_api_password_file,
+    }
+    udm_mock = mocker.patch.object(process_join_data, "UDM")
+    read_from_file_mock = mocker.patch.object(
+        process_join_data,
+        "read_from_file",
+        return_value=udm_api_password,
+    )
+    mocker.patch.object(process_join_data, "App")
+
+    result = runner.invoke(
+        process_join_data.cli_app,
+        ["example-data.yaml"],
+        catch_exceptions=False,
+        env=env,
+    )
+
+    read_from_file_mock.assert_called_once_with(udm_api_password_file)
+    udm_mock.http.assert_called_once_with(udm_api_url, udm_api_user, udm_api_password)
     assert result.exit_code == 0
