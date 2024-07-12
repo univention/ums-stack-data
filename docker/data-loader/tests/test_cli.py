2e1a6485941e18016211bc56908165dffb8156ca
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-12 10:02:25 +0200

    feat: Allow to enable logging of the merged context

--- docker/data-loader/tests/test_cli.py
+++ docker/data-loader/tests/test_cli.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
+import logging
+
 from typer.testing import CliRunner
 
 runner = CliRunner()
@@ -78,4 +80,57 @@ def test_accepts_multiple_template_context_options(process_join_data, mocker):
 
     expected_context = stub_context | stub_context2
     assert app_mock.call_args.kwargs["template_context"] == expected_context
+    assert result.exit_code == 0
+
+
+def test_does_not_log_context_by_default(process_join_data, mocker, caplog):
+    mocker.patch.object(process_join_data, "_connect_to_udm")
+    stub_context = {"stub_name": "stub_value"}
+    stub_context2 = {"stub_name2": "stub_value2"}
+    mocker.patch.object(
+        process_join_data,
+        "load_context",
+        side_effect=[stub_context, stub_context2],
+    )
+
+    with caplog.at_level(logging.DEBUG, logger="app"):
+        result = runner.invoke(
+            process_join_data.cli_app,
+            [
+                "example-data.yaml",
+                "--template-context",
+                "context.yaml",
+                "--template-context",
+                "context2.yaml",
+            ],
+        )
+
+    assert "Merged context" not in caplog.text
+    assert result.exit_code == 0
+
+
+def test_allows_to_log_the_context(process_join_data, mocker, caplog):
+    mocker.patch.object(process_join_data, "_connect_to_udm")
+    stub_context = {"stub_name": "stub_value"}
+    stub_context2 = {"stub_name2": "stub_value2"}
+    mocker.patch.object(
+        process_join_data,
+        "load_context",
+        side_effect=[stub_context, stub_context2],
+    )
+
+    with caplog.at_level(logging.DEBUG, logger="app"):
+        result = runner.invoke(
+            process_join_data.cli_app,
+            [
+                "example-data.yaml",
+                "--log-context",
+                "--template-context",
+                "context.yaml",
+                "--template-context",
+                "context2.yaml",
+            ],
+        )
+
+    assert "Merged context" in caplog.text
     assert result.exit_code == 0
