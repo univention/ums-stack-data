5c711b7c0dfec8d4a1b98ebd1a1085bf5516a836
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-12 09:02:45 +0200

    fix: Simple merge of context values

--- docker/data-loader/tests/test_deep_merge.py
+++ docker/data-loader/tests/test_deep_merge.py
@@ -1,2 +1,11 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
+
+
+def test_merges_simple_values(process_join_data):
+    target = {"a": "stub_value"}
+    source = {"b": "stub_value"}
+
+    result = process_join_data.deep_merge(target, source)
+
+    assert result == {"a": "stub_value", "b": "stub_value"}
