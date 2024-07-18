e8ee92a2660efcac6afcbf966a69d7445da2a9b0
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-18 09:15:53 +0200

    test: Verify that user can override values in template context

--- tests/test_template_context.py
+++ tests/test_template_context.py
@@ -13,10 +13,27 @@ def test_user_adds_value(helm, chart_path):
     )
     result = helm.helm_template(chart_path, values)
     context_secret = helm.get_resource(
-        result, name="release-name-stack-data-ums-context"
+        result,
+        name="release-name-stack-data-ums-context",
     )
     context_data = _get_template_context_data(context_secret)
     assert context_data["stubName"] == "stub-value"
+
+
+def test_user_overrides_value(helm, chart_path):
+    values = safe_load(
+        """
+        templateContext:
+          ldapBaseDn: "dc=testsuite,dc=test"
+        """,
+    )
+    result = helm.helm_template(chart_path, values)
+    context_secret = helm.get_resource(
+        result,
+        name="release-name-stack-data-ums-context",
+    )
+    context_data = _get_template_context_data(context_secret)
+    assert context_data["ldapBaseDn"] == "dc=testsuite,dc=test"
 
 
 def _get_template_context_data(context_secret):
