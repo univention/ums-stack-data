db905c6ed5aff980b0551a2074037523501e02c0
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2024-07-18 09:06:16 +0200

    test: Verify that user can add values into template context

---
+++ tests/test_template_context.py
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Univention GmbH
+
+from yaml import safe_load
+
+
+def test_user_adds_value(helm, chart_path):
+    values = safe_load(
+        """
+        templateContext:
+          stubName: "stub-value"
+        """,
+    )
+    result = helm.helm_template(chart_path, values)
+    context_secret = helm.get_resource(
+        result, name="release-name-stack-data-ums-context"
+    )
+    context_data = _get_template_context_data(context_secret)
+    assert context_data["stubName"] == "stub-value"
+
+
+def _get_template_context_data(context_secret):
+    context_yaml = context_secret["stringData"]["context.yaml"]
+    context_data = safe_load(context_yaml)
+    return context_data
