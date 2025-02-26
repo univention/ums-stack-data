f0b922428839e8b96951cae0e6cfab8a94f5dc0d
Johannes Lohmer <lohmer@univention.de>
2025-02-26 10:19:12 +0100

    refactor: Adapt unittests after code refactoring

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,26 +1,9 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
-import importlib.util
-import os
-import os.path
 import textwrap
 
 import pytest
-
-
-@pytest.fixture
-def process_join_data():
-    """Provide "process-join-data.py" as a module."""
-    module_name = "process_join_data"
-    module_path = "./src/process-join-data.py"
-    spec = importlib.util.spec_from_file_location(
-        module_name,
-        os.path.join(module_path),
-    )
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
 
 
 @pytest.fixture
--- tests/unit/test_deep_merge.py
+++ tests/unit/test_deep_merge.py
@@ -1,38 +1,40 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
+from univention.data_loader.cli import deep_merge
 
-def test_merges_simple_values(process_join_data):
+
+def test_merges_simple_values():
     target = {"a": "stub_value"}
     source = {"b": "stub_value"}
 
-    result = process_join_data.deep_merge(target, source)
+    deep_merge(target, source)
 
-    assert result == {"a": "stub_value", "b": "stub_value"}
+    assert target == {"a": "stub_value", "b": "stub_value"}
 
 
-def test_merges_nested_value(process_join_data):
+def test_merges_nested_value():
     target = {"a": {"sub_a": "sub_a_value"}}
     source = {"a": {"merged_a": "merged_a_value"}}
 
-    result = process_join_data.deep_merge(target, source)
+    deep_merge(target, source)
 
-    assert result == {"a": {"sub_a": "sub_a_value", "merged_a": "merged_a_value"}}
+    assert target == {"a": {"sub_a": "sub_a_value", "merged_a": "merged_a_value"}}
 
 
-def test_replaces_lists(process_join_data):
+def test_replaces_lists():
     target = {"a_list": ["a"]}
     source = {"a_list": ["b"]}
 
-    result = process_join_data.deep_merge(target, source)
+    deep_merge(target, source)
 
-    assert result == {"a_list": ["b"]}
+    assert target == {"a_list": ["b"]}
 
 
-def test_set_a_value_to_none(process_join_data):
+def test_set_a_value_to_none():
     target = {"a": "stub_value"}
     source = {"a": None}
 
-    result = process_join_data.deep_merge(target, source)
+    deep_merge(target, source)
 
-    assert result == {"a": None}
+    assert target == {"a": None}
