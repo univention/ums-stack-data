42df480ffa99532d899ade73b8869690af0ab148
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-17 14:39:15 +0200

    test: Leverage new fixture "chart" from pytest-helm

--- tests/chart/utils.py
+++ tests/chart/utils.py
@@ -12,8 +12,8 @@ def findall(data, path):
     return jsonpath.match(path, data).obj
 
 
-def get_containers_of_job(helm, result):
-    manifest = helm.get_resource(result, kind="Job")
+def get_containers_of_job(result):
+    manifest = result.get_resource(kind="Job")
     init_containers = findall(manifest, "spec.template.spec.initContainers")
     containers = findall(manifest, "spec.template.spec.containers")
     return init_containers + containers
