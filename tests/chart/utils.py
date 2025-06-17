2f3c1982a7befde82415c3240f5602ae348bd9c2
Johannes Bornhold <johannes.bornhold.extern@univention.de>
2025-06-17 14:47:43 +0200

    test: Replace findone and findall by methods from the pytest-helm yaml objects

--- tests/chart/utils.py
+++ tests/chart/utils.py
@@ -1,19 +1,8 @@
 # SPDX-FileCopyrightText: 2024-2025 Univention GmbH
 # SPDX-License-Identifier: AGPL-3.0-only
 
-import jsonpath
-
-
-def findone(data, path):
-    return jsonpath.match(path, data).obj
-
-
-def findall(data, path):
-    return jsonpath.match(path, data).obj
-
-
 def get_containers_of_job(result):
     manifest = result.get_resource(kind="Job")
-    init_containers = findall(manifest, "spec.template.spec.initContainers")
-    containers = findall(manifest, "spec.template.spec.containers")
+    init_containers = manifest.findone("spec.template.spec.initContainers")
+    containers = manifest.findone("spec.template.spec.containers")
     return init_containers + containers
